10ee761979dae96b271f30d808c5696d634d4b16
tvtsolov <65929968+tvtsolov@users.noreply.github.com>
2024-04-06 00:03:33 +0300

    notes updates

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-123
+
+friends = ["Harry", "Sam", "Voldemort", "Rob", "Sirius"]
+other_friends = ["Holly", "Berry", "Samuel"]
+print(friends)
+
+print(friends[1:3])  # excludes the element on the last index
+friends.extend(other_friends)
+print(friends)
+friends.append("another guy")
+print(friends)
+friends.insert(3, 50000)
+print(friends)
+
